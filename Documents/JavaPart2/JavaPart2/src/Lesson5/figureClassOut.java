300f4e762f230776f5db05dc752ab242ec00e298
ArseniyChern <SyzzleHD@gmail.com>
2017-02-09 21:21:47 +0200

    Update

--- Documents/JavaPart2/JavaPart2/src/Lesson5/figureClassOut.java
+++ Documents/JavaPart2/JavaPart2/src/Lesson5/figureClassOut.java
@@ -8,24 +8,27 @@ import Lesson1and2.CPoint;
 import Lesson1and2.Triangle;
 import Lesson4.DrawHouse;
 public class figureClassOut {
-
+	static ObjectOutputStream out;
+	static ObjectInputStream in;
+	static MainFigures g;
+	static Figure[] a;
+	static Figure[] b;
+	static DrawHouse h;
+	
 	public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
-		ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("res"+File.separator+"textIn"));
-		ObjectInputStream in = new ObjectInputStream(new FileInputStream("res"+File.separator+"textIn"));
-		MainFigures g = new MainFigures();
-		Figure[] a = g.masCPoint;
+		
+		out = new ObjectOutputStream(new FileOutputStream("res"+File.separator+"textIn"));
+		in = new ObjectInputStream(new FileInputStream("res"+File.separator+"textIn"));
+		g = new MainFigures();
+		a = g.masCPoint;
 		
 		out.writeObject(a);
 		out.flush();
 		out.close();
-		//This projoject is worth 0.00 Dollars.
-		Figure[] b = (Figure[]) in.readObject();
-		DrawHouse h = new DrawHouse(b);
+		
+		b = (Figure[]) in.readObject();
+		h = new DrawHouse(b);
 		h.main(args);
-		
-		
-		
-		
 		
 	}
 
