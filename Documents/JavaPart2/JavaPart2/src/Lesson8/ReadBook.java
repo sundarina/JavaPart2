3e9d099397ecc55efc521640c3f3b2a9396d4a34
ArseniyChern <SyzzleHD@gmail.com>
2017-02-17 23:10:35 +0200

    Update

--- Documents/JavaPart2/JavaPart2/src/Lesson8/ReadBook.java
+++ Documents/JavaPart2/JavaPart2/src/Lesson8/ReadBook.java
@@ -13,137 +13,167 @@ import org.xml.sax.helpers.DefaultHandler;
 public class ReadBook extends DefaultHandler {
 	private StringBuffer stringBuffer;
 	ArrayList books = new ArrayList<Book>();
+	int b = 0;
 
 	public static void main(String[] args) {
-	
-		
-		
-		
-		
-		
 
-
-		// Создаем экземпляр обработчика SAX
 		DefaultHandler defaultHandler = new ReadBook();
 
-		// Создаем экземпляр класса SAXParserFactory
 		SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
 		try {
 
-			// Создаем экземпляр класса SAXParser
 			SAXParser Sax_Parser = saxParserFactory.newSAXParser();
 
-			// Запуск парсера XML файла
 			Sax_Parser.parse(new File("res" + File.separator + "booksIn.xml"), defaultHandler);
 			
+
 		} catch (Exception e) {
-			
+
 			e.printStackTrace();
 		}
 	}
 
-	/**
-	 * Обрабатывает событие начала документа
-	 */
 	public void startDocument() throws SAXException {
-		//System.out.println("<?xml version = '1.0' encoding = 'UTF-8'?>");
+
 	}
 
-	/**
-	 * Обрабатывает событие конца документа
-	 */
-	public void endDocument() throws SAXException {}
+	public void endDocument() throws SAXException {
 
-	/**
-	 * Обрабатывает событие начала элемента <...>
-	 */
-	public void startElement(String namespaceURI, String localName, String qName, Attributes attrs) throws SAXException {
-		//displayBufferText();
-		
-		if ("".equals(localName))
+	}
+
+	public void startElement(String namespaceURI, String localName, String qName, Attributes attrs)
+			throws SAXException {
+
+		if ("".equals(localName)) {
 			localName = qName;
-		
-		//System.out.print("<" + localName);
+
+		}
+		if(qName.equals("BookName")) {
+			b = 1;
+		} else if(qName.equals("BookPrice")) {
+			b = 2;
+		} else if(qName.equals("BookAuthor")) {
+			b = 3;
+		} else if  (qName.equals("Book")) {
+			books.add(new Book("","",0));
+			b = 4;
+			((Book)books.get(books.size()-1)).setQuantity(Integer.parseInt(attrs.getValue(0)));
+		} else {
+			b = 4;
+		}
+
 		if (attrs != null) {
-			
-			// Получаем количество атрибутов элемента
+
 			for (int i = 0; i < attrs.getLength(); i++) {
-				
-			
+
 				String aName = attrs.getLocalName(i);
-				
-				
-				String Author = attrs.getValue(i+++1);
-				//int price = Integer.parseInt(attrs.getValue(i+++1));
-				books.add(new Book(aName,Author,3));
-				//System.out.println("Hi"+books.get(books.size()-1));
+
+				String Author = attrs.getValue(i);
 				
 				if ("".equals(aName)) {
 					aName = attrs.getQName(i);
+					
 				}
-				//System.out.print(" ");
-			
-				
-				//System.out.print(aName + "=\"" + attrs.getValue(i) + "\"");
+
 			}
 		}
-		//System.out.print(">");
+
 	}
 
-	/**
-	 * Обрабатывает событие конца элемента </...>
-	 */
 	public void endElement(String namespaceURI, String localName, String qName) throws SAXException {
-		//displayBufferText();
+		 displayBufferText();
 		if ("".equals(localName))
 			localName = qName;
-		//System.out.print("</" + localName + ">");
+
 	}
 
-	/**
-	 * Обрабатывает событие символьных данных текстового элемента
-	 * <...>some_text</...>
-	 */
 	public void characters(char buf[], int offset, int len) throws SAXException {
 		String s = new String(buf, offset, len);
-		System.out.println();
-		System.out.println("========"+s+"=======");
-		// если строковый буфер равен null, создаем новый
+
+		s = s.replaceAll("  ", "");
+		s = s.replaceAll("\n", "");
+		s = s.replaceAll("	", "");
+		if (!(s.isEmpty())) {
+
+			Book f = new Book("", "", 0);
+			switch (b) {
+			case 1: ((Book)books.get(books.size()-1)).setName(s);
+				break;
+			case 2: ((Book)books.get(books.size()-1)).setPrice(Integer.parseInt(s));
+				break;
+			case 3: ((Book)books.get(books.size()-1)).setAuthor(s); System.out.println(books.get(books.size()-1));
+				break;
+			case 4:
+				break;
+			}
+
+		}
+
 		if (stringBuffer == null) {
-			//stringBuffer = new StringBuffer(s);
+
 		} else {
-			
-			// Добавляем символы в строковый буфер
-			//stringBuffer.append(s);
+
 		}
 	}
 
-	/**
-	 * Выводит текст, собранный в строковом буфере
-	 */
 	private void displayBufferText() {
 		if (stringBuffer != null) {
-			//System.out.print(stringBuffer.toString());
+
 			stringBuffer = null;
 		}
 	}
 }
 
 class Book {
-	String Name;
-	String Author;
-	int Price;
+	private String Name;
+	private String Author;
+	private int Price;
+	private int quantity;
+
+	public int getQuantity() {
+		return quantity;
+	}
+
 	
+
+	public void setQuantity(int string) {
+		this.quantity = string;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	public String getAuthor() {
+		return Author;
+	}
+
+	public void setAuthor(String author) {
+		Author = author;
+	}
+
+	public int getPrice() {
+		return Price;
+	}
+
+	public void setPrice(int price) {
+		Price = price;
+	}
+
 	public Book(String name, String author, int price) {
 		super();
 		this.Name = name;
 		this.Author = author;
 		this.Price = price;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "Name = " + Name+" | Author = "+Author+" | Price = " + Price;
-		
+		return "Name = " + Name + " | Author = " + Author + " | Price = " + Price + " | Quantity = "+quantity;
+
 	}
 }
